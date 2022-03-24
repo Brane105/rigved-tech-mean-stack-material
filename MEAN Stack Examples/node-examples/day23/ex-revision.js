3a59b0215dc1064522d3feb9a0e0dfd54fb7959e
Kishor <kishor1124@gmail.com>
2022-03-24 15:07:59 +0530

    node examples

--- MEAN Stack Examples/node-examples/day23/ex-revision.js
+++ MEAN Stack Examples/node-examples/day23/ex-revision.js
@@ -3,8 +3,18 @@ let read = require("readline-sync");
 
 // firstly you need to assume there's is already an array in file
 let data = fs.readFileSync('emp.json');
-let dataString = data.toString();
-console.log("Data String: "+dataString);
+let dataString = data.toString(); // converts the byte stream to string
+let jsArray = undefined;
+if(dataString.length < 1 || dataString == "") {
+    jsArray = []; // creating javascript array
+} else {
+    jsArray = JSON.parse(dataString); // json array will be converted to javascript array
+}
 
-//let name = read.question('Enter name: ');
-//let age = read.questionInt('Enter age: ');
+let n = read.question('Enter name: ');
+let a = read.questionInt('Enter age: ');
+let emp = {name : n, age : a};
+jsArray.push(emp); // pushes javascript object to javascript array
+let jsonArray = JSON.stringify(jsArray); // converts javascript array to json array
+// write jsonArray to the file
+fs.writeFileSync("emp.json", jsonArray);
